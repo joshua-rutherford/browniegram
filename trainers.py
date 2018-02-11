83f67197a9e46bcf10f007a871255437d1aa06a9
Joshua Mark Rutherford <joshua.rutherford@deciphernow.com>
2018-02-11 16:47:30 -0500

    S

--- trainers.py
+++ trainers.py
@@ -10,7 +10,8 @@ def opencv(model, directory):
         for label in directories:
             files = os.listdir(os.path.join(directory, label))
             for file in files:
-                face = images.resize(cv2.imread(os.path.join(directory, label, file), cv2.IMREAD_GRAYSCALE), 92, 112, cv2.INTER_LANCZOS4)
+                a = cv2.imread(os.path.join(directory, label, file), cv2.IMREAD_GRAYSCALE)
+                face = images.resize(a, 92, 112, cv2.INTER_LANCZOS4)
                 faces.append(face)
                 labels.append(label)
         model.train(images, labels)
