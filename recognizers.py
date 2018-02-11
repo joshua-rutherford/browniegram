484a07ba1f90b8220340d5f96ed5f2d98c34f133
Joshua Mark Rutherford <joshua.rutherford@deciphernow.com>
2018-02-11 17:16:00 -0500

    S

--- recognizers.py
+++ recognizers.py
@@ -1,6 +1,6 @@
 def opencv(model, threshold):
     def recognize(image):
-        label, confidence = model.predict(image)
+        label, confidence = model.predict(images.resize(image, (92, 112), cv2.INTER_LANCZOS4))
         return label if confidence < threshold else None
     return recognize
 
