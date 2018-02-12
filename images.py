fbabf7a03aad3bf3549b551850d145ead5cd5365
Joshua Mark Rutherford <joshua.rutherford@deciphernow.com>
2018-02-11 19:05:37 -0500

    S

--- images.py
+++ images.py
@@ -3,12 +3,12 @@ import time
 
 def scan(interval, capture, detect, callback):
     while True:
-        image = capture()
+        image = cv2.cvtColor(capture, cv2.COLOR_RGB2GRAY)
         detections = detect(image)
         if len(detections) == 1:
             left, top, width, height = detections[0]
             print("{0}, {1}, {2}, {3}".format(left, top, width, height))
-            callback(crop(image[0], left, top, width, height))
+            callback(crop(image[0], left, top, 92, 112))
         else:
             time.sleep(interval)
 
