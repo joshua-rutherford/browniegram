f0755d5a1c232e48a3228ad37a369197c1b4ea37
Joshua Mark Rutherford <joshua.rutherford@deciphernow.com>
2018-02-11 20:15:04 -0500

    S

--- recognizers.py
+++ recognizers.py
@@ -4,6 +4,7 @@ import images
 def opencv(model, threshold):
     def recognize(image):
         label, confidence = model.predict(image)
+        print("I think it is {0} and I have a confidence of {0}", label, confidence)
         return label if confidence < threshold else None
     return recognize
 
